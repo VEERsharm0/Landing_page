51fd2902ced549a2f72de362e00979255e6d5515
Sarvesh Sharma <snps042006@gmail.com>
2025-10-25 12:50:40 +0530

    Remove ngrok origin from CORS

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:8084', 'https://4d79d86c1186.ngrok-free.app'],
+  origin: ['http://localhost:8080', 'http://localhost:8081', 'http://localhost:8084'],
   credentials: true
 }));
 app.use(express.json());

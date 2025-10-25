54d85d3d29c39476c22b93381c2e9dc36bd14362
Sarvesh Sharma <snps042006@gmail.com>
2025-10-25 12:42:28 +0530

    Update CORS to allow production frontend URL via env var

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:8081', 'https://4d79d86c1186.ngrok-free.app'],
+  origin: ['http://localhost:8081', 'https://4d79d86c1186.ngrok-free.app', process.env.FRONTEND_URL],
   credentials: true
 }));
 app.use(express.json());
